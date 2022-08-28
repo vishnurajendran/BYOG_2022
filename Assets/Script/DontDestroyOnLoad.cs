e18e7852d7ab0a0c211f803b4a3a956e7dfb6b75
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2022-08-28 17:27:11 +0530

    Update DontDestroyOnLoad.cs

--- Assets/Script/DontDestroyOnLoad.cs
+++ Assets/Script/DontDestroyOnLoad.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    private static DontDestroyOnLoad instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
